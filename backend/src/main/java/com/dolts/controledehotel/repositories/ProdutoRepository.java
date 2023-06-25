f5e7c4968a93acbf07bfc6c3236f45b4527ac6d3
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-06-25 15:03:24 -0300

    Pensando em nova implementação

--- backend/src/main/java/com/dolts/controledehotel/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dolts.controledehotel.repositories;
-
-import com.dolts.controledehotel.models.ProdutoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
-}
--- backend/src/main/java/com/dolts/controledehotel/repositories/SaidaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dolts.controledehotel.repositories;
-
-import com.dolts.controledehotel.models.SaidaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaidaRepository extends JpaRepository<SaidaModel, Long> {
-}
