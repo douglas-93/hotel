2ddbe5bfbb9a92f8a8cb5ed458cfa6fcc4389448
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-07-05 21:48:25 -0300

    Implementado a visualização das diárias com o total, o total dos produtos consumidos e a possibilidade de aplicar desconto no total

--- frontend/src/app/shared/components/nota-form/nota-form.component.ts
+++ frontend/src/app/shared/components/nota-form/nota-form.component.ts
@@ -2,12 +2,12 @@ import {Component, NgModule, ViewChild} from '@angular/core';
 import {
 	DxButtonModule,
 	DxDataGridModule,
-	DxLoadPanelModule,
+	DxLoadPanelModule, DxRadioGroupComponent, DxRadioGroupModule,
 	DxSelectBoxComponent,
-	DxSelectBoxModule
+	DxSelectBoxModule, DxTextBoxModule
 } from "devextreme-angular";
 import {ToolbarModule} from "../toolbar/toolbar.component";
-import {CurrencyPipe} from "@angular/common";
+import {CurrencyPipe, NgIf} from "@angular/common";
 import {ProdutoModel} from "../../models/produto.model";
 import {forkJoin} from "rxjs";
 import {ReservaModel} from "../../models/reserva.model";
@@ -16,6 +16,8 @@ import {ProdutoService} from "../../services/produto.service";
 import {ConsumoService} from "../../services/consumo.service";
 import {ConsumoModel} from "../../models/consumo.model";
 import notify from "devextreme/ui/notify";
+import DevExpress from "devextreme";
+import data = DevExpress.data;
 
 @Component({
 	selector: 'app-nota-form',
@@ -25,12 +27,19 @@ import notify from "devextreme/ui/notify";
 export class NotaFormComponent {
 
 	@ViewChild('reserva') reservaSelectBox: DxSelectBoxComponent;
+	@ViewChild('tipoDesconto') tipoDesconto: DxRadioGroupComponent;
 
 	loadingVisible: boolean = false;
-	gridData: { produto: ProdutoModel, quantidade: number, dataConsumo: Date }[];
-	gridDataSelected: { produto: ProdutoModel, quantidade: number, dataConsumo: Date } | undefined;
+	gerarVisible: boolean = false;
+	temDesconto: boolean = false;
+	gridData: { produto: ProdutoModel, quantidade: number, dataConsumo: Date, valor: number }[];
 	reservas: ReservaModel[];
 	produtos: ProdutoModel[];
+	datasDaReserva: { data: Date, valor: number }[] = [];
+	totalDaNota: number;
+	radioOptions: string[] = ['Valor', 'Percentual'];
+	totalDaNotaComDesconto: number;
+	valorDesconto: string;
 
 
 	constructor(private reservaService: ReservaService,
@@ -55,19 +64,16 @@ export class NotaFormComponent {
 			});
 	}
 
-	selecionaProduto(e: any) {
-		e.component.byKey(e.currentSelectedRowKeys[0]).done(p => {
-			if (p) {
-				this.gridDataSelected = p;
-			}
-		});
-	}
-
 	salvarNota() {
 
 	}
 
 	calculaConsumo() {
+		if (this.reservaSelectBox.selectedItem === null || this.reservaSelectBox.selectedItem === undefined) {
+			notify('É necessário escolher uma reserva', 'error', 3000);
+			return
+		}
+		this.gerarVisible = true
 		let id = this.reservaSelectBox.selectedItem.id
 		this.consumoService.getConsumo(id).subscribe(res => {
 			if (res.status === 200) {
@@ -81,13 +87,77 @@ export class NotaFormComponent {
 
 					let prod = this.produtos.find(p => p.id === c.produtoId)
 
-					this.gridData.push({produto: prod!, quantidade: c.quantidade, dataConsumo: data})
+					let valorTotal = c.quantidade * prod?.preco!
+					this.gridData.push({produto: prod!, quantidade: c.quantidade, dataConsumo: data, valor: valorTotal})
+					this.totalDaNota += valorTotal
 				})
 				notify('Calculo realizado com sucesso!', 'success', 3000)
 			}
 		})
+
+		let reservaSelecionada: ReservaModel = this.reservaSelectBox.selectedItem
+
+		const dataInicial = new Date(reservaSelecionada.dataEntrada);
+		const dataFinal = new Date(reservaSelecionada.dataSaida);
+
+		this.datasDaReserva = []
+
+		this.geraDiasDiarias(dataInicial, dataFinal).forEach(d => {
+			this.datasDaReserva.push({data: d, valor: reservaSelecionada.quarto.valor})
+			this.totalDaNota += reservaSelecionada.quarto.valor
+		});
 	}
 
+	geraDiasDiarias(dataInicial: Date, dataFinal: Date): Date[] {
+		const days: Date[] = [];
+		let currentDate = dataInicial;
+
+		while (currentDate <= dataFinal) {
+			days.push(new Date(currentDate));
+			currentDate = new Date(currentDate.getTime() + 86400000); // adiciona 24 horas (em milissegundos)
+		}
+
+		return days;
+	}
+
+
+	limparConsumoGerado() {
+		this.gerarVisible = false
+		this.gridData = []
+		this.datasDaReserva = []
+		this.totalDaNota = 0
+	}
+
+	aplicaDesconto() {
+		this.totalDaNotaComDesconto = 0
+		this.temDesconto = false
+		if (this.tipoDesconto.value === null) {
+			notify('Selecione o tipo de desconto', 'error', 3000)
+			return;
+		}
+
+		if (this.tipoDesconto.value === 'Valor') {
+			let desc = Number.parseFloat(this.valorDesconto = this.valorDesconto.replace('.', ','))
+			if (desc > this.totalDaNota) {
+				notify('O desconto não pode ser maior que o total', 'error', 3000)
+				return;
+			}
+			this.totalDaNotaComDesconto = this.totalDaNota - desc
+			this.temDesconto = true
+			return;
+		}
+
+		if (this.tipoDesconto.value === 'Percentual') {
+			let desc = Number.parseFloat(this.valorDesconto.replace(',', '.'))
+			if (desc > 100) {
+				notify('O desconto não pode ser maior que 100%', 'error', 3000)
+				return;
+			}
+			this.totalDaNotaComDesconto = this.totalDaNota * (1 - (desc / 100))
+			this.temDesconto = true
+			return;
+		}
+	}
 }
 
 
@@ -98,7 +168,10 @@ export class NotaFormComponent {
 		DxSelectBoxModule,
 		DxButtonModule,
 		DxDataGridModule,
-		CurrencyPipe
+		CurrencyPipe,
+		NgIf,
+		DxTextBoxModule,
+		DxRadioGroupModule
 	],
 	declarations: [NotaFormComponent],
 	exports: [NotaFormComponent]
