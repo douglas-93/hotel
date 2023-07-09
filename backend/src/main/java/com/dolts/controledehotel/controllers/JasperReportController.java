f477920fef88229ec99f5fe46c3331a126cff08b
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-07-09 13:59:04 -0300

    Começando a implementação de relatórios

---
+++ backend/src/main/java/com/dolts/controledehotel/controllers/JasperReportController.java
@@ -0,0 +1,46 @@
+package com.dolts.controledehotel.controllers;
+
+import com.dolts.controledehotel.models.HospedeModel;
+import com.dolts.controledehotel.services.HospedeService;
+import com.dolts.controledehotel.services.JasperReportService;
+import com.dolts.controledehotel.utils.ListQueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class JasperReportController {
+
+    private final JasperReportService reportService;
+    private final HospedeService hospedeService;
+
+    @Autowired
+    public JasperReportController(JasperReportService reportService, HospedeService hospedeService) {
+        this.reportService = reportService;
+        this.hospedeService = hospedeService;
+    }
+
+    @GetMapping("/relatorio")
+    public ResponseEntity<byte[]> gerarRelatorio() {
+        Map<String, Object> parametros = new HashMap<>();
+
+        List<HospedeModel> dadosDoRelatorio = hospedeService.findAll();
+
+        dadosDoRelatorio.forEach(h -> System.out.println(h.getNome() + h.getId()));
+
+        byte[] relatorio = reportService.generateReport("Clientes", parametros, dadosDoRelatorio);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDisposition(ContentDisposition.attachment().filename("relatorio.pdf").build());
+
+        return new ResponseEntity<>(relatorio, headers, HttpStatus.OK);
+    }
+}
+
---
+++ backend/src/main/java/com/dolts/controledehotel/services/JasperReportService.java
@@ -0,0 +1,43 @@
+package com.dolts.controledehotel.services;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class JasperReportService {
+
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    public JasperReportService(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    public byte[] generateReport(String reportName, Map<String, Object> parameters, List<?> dataSource) {
+        try {
+            InputStream reportStream = resourceLoader.getResource("classpath:relatorios/" + reportName + ".jasper").getInputStream();
+
+            JasperReport jasperReport = (JasperReport) net.sf.jasperreports.engine.util.JRLoader.loadObject(reportStream);
+
+//            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(Collections.singletonList(dataSource)));
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(dataSource));
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+
+            return outputStream.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
